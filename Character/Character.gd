ee4361faa70e16958f4c032cb12511a0950f5a67
Jadale18 <95486525+Jadale18@users.noreply.github.com>
2024-07-19 16:25:38 -0700

    Added life system and game over function in player script

--- Character/Character.gd
+++ Character/Character.gd
@@ -1,25 +1,22 @@
 extends CharacterBody2D
 
+var max_life = 4
+var current_life_total = 4
 
 const SPEED = 150.0
 const JUMP_VELOCITY = -300.0
-@export_range(-1, 1, 2) var gravity_multiplier = 1
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+@export_range(-1, 1, 2) var gravity_multiplier = 1
 @onready var gravity = ProjectSettings.get_setting("physics/2d/default_gravity") * gravity_multiplier
 
 
 func _physics_process(delta):
-	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and (is_on_floor() or is_on_ceiling()):
 		velocity.y = JUMP_VELOCITY * gravity_multiplier
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
@@ -27,3 +24,17 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	take_damage()
+	check_gameover()
+
+
+func take_damage():
+	if Input.is_action_just_pressed("Take Damage"):
+		print('dmg')
+		current_life_total -= 1
+		$CanvasLayer/HBoxContainer.get_child(current_life_total).visible = false
+
+func check_gameover():
+	if current_life_total == 0:
+		print('game over')
+		
