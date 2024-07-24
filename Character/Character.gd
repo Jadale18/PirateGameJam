729b4ebc474a2c6db5e63ed165d80438f8d278dc
Jadale18 <95486525+Jadale18@users.noreply.github.com>
2024-07-24 15:27:34 -0700

    More Animation work

--- Character/Character.gd
+++ Character/Character.gd
@@ -9,6 +9,11 @@ var current_life = 10
 @export_range(-1, 1, 2) var gravity_multiplier = 1
 @onready var gravity = ProjectSettings.get_setting("physics/2d/default_gravity") * gravity_multiplier
 var animating = false
+var falling = false
+var landing = false
+var jumping = false
+var dead = false
+var checking_input = true
 
 func _ready():
 	$Anims.play("Idle")
@@ -18,14 +23,20 @@ func _physics_process(delta):
 		handle_anims()
 	if not is_on_floor():
 		velocity.y += gravity * delta
-	if current_life == 0:
+	if current_life == 0 and not dead:
 		$Anims.play("Death")
+		dead = true
+		checking_input = false
+	if is_on_floor() and falling and not animating and not jumping:
+		landing = true
+		$Anims.play("Land")
+		falling = false
 
-	if Input.is_action_just_pressed("ui_accept") and (is_on_floor() or is_on_ceiling()):
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor() and checking_input:
 		velocity.y = JUMP_VELOCITY * gravity_multiplier
 
 	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
+	if direction and checking_input:
 		velocity.x = direction * SPEED
 		if direction < 0:
 			$Anims.flip_h = true
@@ -35,9 +46,10 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
-	if Input.is_action_just_pressed("Take Damage"):
+	if Input.is_action_just_pressed("Take Damage") and is_on_floor() and checking_input:
 		animating = true
 		$Anims.play("Self_Damage")
+		checking_input = false
 		take_damage()
 	
 	Global.character_position = global_position
@@ -50,23 +62,32 @@ func _on_area_2d_area_entered(area):
 	if "Spike" in area.name:
 		animating = true
 		$Anims.play("Damaged")
+		checking_input = false
 		take_damage()
 
 func handle_anims():
 	if is_on_floor() and velocity.x != 0:
 		$Anims.play("Run")
-	elif is_on_floor() and velocity.x == 0:
+		landing = false
+	elif is_on_floor() and velocity.x == 0 and not landing:
 		$Anims.play("Idle")
-	if Input.is_action_just_pressed("ui_accept"):
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		$Anims.play("Jump")
+		landing = false
+		jumping = true
 
-
-
-
-func _on_anims_animation_looped():
+func _on_anims_animation_finished():
 	if $Anims.animation == "Jump":
 		$Anims.play("Air_Look_Down")
+		jumping = false
+		falling = true
 	elif $Anims.animation == "Air_Look_Down":
 		$Anims.play("Air_Idle")
+	elif $Anims.animation == "Land":
+		landing = false
 	elif $Anims.animation in ["Damaged", "Self_Damage"]:
 		animating = false
+		checking_input = true
+		if is_on_floor():
+			falling = false
+
