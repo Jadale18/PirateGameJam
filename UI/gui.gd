6cf13667c4265d407d3725589a84e0a541efa2fa
Jadale18 <95486525+Jadale18@users.noreply.github.com>
2024-07-30 13:03:49 -0700

    More health bar code

--- UI/gui.gd
+++ UI/gui.gd
@@ -1,25 +1,32 @@
 extends Node2D
 
-var max_life = 10
-var current_life_total = 10
+var max_life = 4
+var current_life_total = 4
+
+func _ready():
+	for child in $CanvasLayer/HBoxContainer.get_children():
+		child.get_child(0).play("Empty")
+		child.get_child(1).play("Idle")
 
 func _process(delta):
 	if current_life_total == 0:
 		pass
+	if Global.current_life_total == current_life_total - 1:
+		damage_taken()
 
-func _on_character_damage_taken():
+func damage_taken():
 	current_life_total -= 1
-	$CanvasLayer/HBoxContainer.get_child(current_life_total).visible = false
+	$CanvasLayer/HBoxContainer.get_child(current_life_total).get_child(1).play("Damage")
 
-	$LightTimer.start()
+
+
+
 
 
 
 func _on_shadow_damage_taken():
 	current_life_total -= 1
 	$CanvasLayer/HBoxContainer.get_child(current_life_total).visible = false
-
-	$LightTimer.start()
 
 
 
