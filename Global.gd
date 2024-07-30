7ae6bf4b2eca3db19364798d1efd45a9bc2cec7f
Jadale18 <95486525+Jadale18@users.noreply.github.com>
2024-07-30 16:28:13 -0700

    Scene Transition code

--- Global.gd
+++ Global.gd
@@ -1,6 +1,6 @@
 extends Node
 
-var character_position = Vector2(89,-150)
+var character_position = Vector2(65,-150)
 var current_animation
 var facing
 #var current_level = 1
@@ -9,14 +9,20 @@ var current_life_total = 4
 
 var light_up = false
 
-#func change_level():
-	#var current_level_path = "/root/GameScene/VBoxContainer/TopViewportContainer/TopViewport/CurrentLevel"
-	#var next_path = load(str("res://World/Levels/level", current_level + 1, ".tscn"))
-	#var instance = next_path.instantiate()
-	#instance.name = "CurrentLevel"
-	#get_node("/root/GameScene/VBoxContainer/TopViewportContainer/TopViewport").add_child(instance)
-	#get_node(current_level_path).queue_free()
-	#current_level += 1
+func change_level(new_level, shadow_exit, portal_spawn):
+	call_deferred("_deferred_change_level", new_level, shadow_exit, portal_spawn)
+
+func _deferred_change_level(new_level, shadow_exit, portal_spawn):
+	var subviewport = get_node("/root/GameScene/VBoxContainer/TopViewportContainer/TopViewport")
+	subviewport.get_child(0).queue_free()
+	var next_scene = load(new_level)
+	character_position = portal_spawn
+	subviewport.add_child(next_scene.instantiate())
+	
+	var shadow_subviewport = get_node("/root/GameScene/VBoxContainer/BottomViewportContainer/BottomViewport")
+	shadow_subviewport.get_child(0).queue_free()
+	var sh_next_scene = load(shadow_exit)
+	shadow_subviewport.add_child(sh_next_scene.instantiate())
 
 func damage():
 	current_life_total -= 1
