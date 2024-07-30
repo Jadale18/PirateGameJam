cc0d60ad013b3d41a9fe9d2c5ccb97024daf95d3
Jadale18 <95486525+Jadale18@users.noreply.github.com>
2024-07-30 12:17:25 -0700

    Working on health bar

--- UI/gui.gd
+++ UI/gui.gd
@@ -10,7 +10,7 @@ func _process(delta):
 func _on_character_damage_taken():
 	current_life_total -= 1
 	$CanvasLayer/HBoxContainer.get_child(current_life_total).visible = false
-	$CanvasLayer/Darkness.visible = false
+
 	$LightTimer.start()
 
 
@@ -18,10 +18,8 @@ func _on_character_damage_taken():
 func _on_shadow_damage_taken():
 	current_life_total -= 1
 	$CanvasLayer/HBoxContainer.get_child(current_life_total).visible = false
-	$CanvasLayer/Darkness.visible = false
+
 	$LightTimer.start()
 
 
 
-func _on_light_timer_timeout():
-	$CanvasLayer/Darkness.visible = true
