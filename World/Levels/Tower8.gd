6a20f1841126f37d80ad7a7a5be48db965c40a0b
KonnorMJE <104404170+KonnorMJE@users.noreply.github.com>
2024-07-31 03:21:24 -0700

    Created mechanism to open final door
    Created mechanism to open final door

--- World/Levels/Tower8.gd
+++ World/Levels/Tower8.gd
@@ -5,6 +5,8 @@ extends Node2D
 func _ready():
 	if Global.health_upgrades[7] == 1:
 		$Background/OffOrb.queue_free()
+	if Global.dumb_doors[10] == 1:
+		$FinalDoor.queue_free()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
