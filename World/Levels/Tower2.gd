c4f6f5b80b9dc62074123a2cd14ad06ffdeae6d2
KonnorMJE <104404170+KonnorMJE@users.noreply.github.com>
2024-07-31 02:14:51 -0700

    Updated with Main
    Updated with Main

--- World/Levels/Tower2.gd
+++ World/Levels/Tower2.gd
@@ -6,7 +6,7 @@ func _ready():
 	if Global.health_upgrades[1] == 1:
 		$Background/OffOrb.queue_free()
 	if Global.dumb_doors[1] == 1:
-		
+		pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
